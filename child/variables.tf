d0cdec9852c1b5b2ee2178aaec14990388a2e90a
NickChatman <115037298+NickChatman@users.noreply.github.com>
2023-02-21 13:35:10 -0600

    Update variables.tf

---
+++ child/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+  type = string
+  default ="ami-0dfcb1ef8550277af"
+}
+
+variable "instance" {
+  type = string
+  default = "t2.micro"
+}
+variable "region" {
+  type = string
+  default ="us-east-1"
+  }
